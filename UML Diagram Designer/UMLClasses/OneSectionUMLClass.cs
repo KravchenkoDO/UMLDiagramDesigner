7c2bb763474a80a083ca9e423b3cef8553a791c6
Dmitry <Kravchenkodmo@gmail.com>
2021-04-08 09:03:13 +0300

    OneSectionUMLClass add

---
+++ UML Diagram Designer/UMLClasses/OneSectionUMLClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Diagram_Tool.UMLClasses
+{
+    public class OneSectionUMLClass : AbstractUMLClass
+    {
+        public OneSectionUMLClass()
+        {
+            _pen = new Pen(Color.Blue, 8);
+        }
+        public override void DrawUMLClass(Graphics graphics, Point endPoint)
+        {
+            Width = endPoint.X - StartPoint.X;
+            Height = endPoint.Y - StartPoint.Y;
+
+            graphics.DrawRectangle(_pen, StartPoint.X, StartPoint.Y, Width, Height);
+        }
+    }
+}
---
+++ UML Diagram Designer/UMLClasses/TwoSectionUMLClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Diagram_Tool.UMLClasses
+{
+    public class TwoSectionUMLClass: AbstractUMLClass
+    {
+        public TwoSectionUMLClass()
+        {
+            _pen = new Pen(Color.Red, 8);
+        }
+        public override void DrawUMLClass(Graphics graphics, Point endPoint)
+        {
+            Width = endPoint.X - StartPoint.X;
+            Height = endPoint.Y - StartPoint.Y;
+
+            graphics.DrawRectangle(_pen, StartPoint.X, StartPoint.Y, Width, Height);
+        }
+    }
+}
