0b4690c0d30b522848e75c2bef62bddffca6c0ab
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-04-15 15:29:10 +0300

    Add using

--- UML Diagram Designer/FactoryClasses/RelationshipFactories/AggregationRelationshipFactory.cs
+++ UML Diagram Designer/FactoryClasses/RelationshipFactories/AggregationRelationshipFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UML_Diagram_Designer.ArrowParts;
 using UML_Diagram_Designer.Relationships;
 
 namespace UML_Diagram_Designer.FactoryClasses.RelationshipFactories
--- UML Diagram Designer/FactoryClasses/RelationshipFactories/AssociationRelationshipFactory.cs
+++ UML Diagram Designer/FactoryClasses/RelationshipFactories/AssociationRelationshipFactory.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UML_Diagram_Designer.ArrowParts;
+using UML_Diagram_Designer.Relationships;
 
 namespace UML_Diagram_Designer.FactoryClasses.RelationshipFactories
 {
