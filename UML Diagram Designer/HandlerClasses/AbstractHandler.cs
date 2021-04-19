457dc238f3307452dd6f9023ddfd7b0ff6151875
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-04-19 21:01:45 +0300

    Added blank handler classes

---
+++ UML Diagram Designer/HandlerClasses/AbstractHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UML_Diagram_Designer.HandlerClasses
+{
+    public abstract class AbstractHandler
+    {
+        public abstract void MouseDown(MouseEventArgs e);
+        public abstract void MouseMove(MouseEventArgs e);
+        public abstract void MouseUp(MouseEventArgs e);
+        public abstract void MouseClick(MouseEventArgs e);
+    }
+}
---
+++ UML Diagram Designer/HandlerClasses/ChangeColorAndSizeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UML_Diagram_Designer.HandlerClasses
+{
+    class ChangeColorAndSizeHandler : AbstractHandler
+    {
+        public override void MouseClick(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseDown(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseMove(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseUp(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
